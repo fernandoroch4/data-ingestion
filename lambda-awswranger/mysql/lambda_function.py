be7d7d72818330587a370a1d474597fa461d77ed
Fernando <fernandoroch4@gmail.com>
2023-03-16 22:11:15 -0300

    improvements

--- lambda-awswranger/mysql/lambda_function.py
+++ lambda-awswranger/mysql/lambda_function.py
@@ -6,11 +6,13 @@ import awswrangler as wr
 import boto3
 import pandas as pd
 import pymysql
-from aws_xray_sdk.core import patch_all, xray_recorder
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
-# Patching all libraries to instrument downstream calls
-xray_recorder.configure(service="data-ingestion-mysql", context_missing="IGNORE_ERROR")
-patch_all()
+# from aws_xray_sdk.core import patch_all, xray_recorder
+
+# # Patching all libraries to instrument downstream calls
+# xray_recorder.configure(service="data-ingestion-mysql", context_missing="IGNORE_ERROR")
+# patch_all()
 
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASS = os.getenv("MYSQL_PASS")
@@ -19,11 +21,11 @@ MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
 DESTINATION_PATH = os.getenv("DESTINATION_PATH")
 LIMIT = int(os.getenv("LIMIT", 100))
-PARAMETER_NAME = os.getenv("OFFSET_PARAMETER_NAME")
+OFFSET_PARAMETER_NAME = os.getenv("OFFSET_PARAMETER_NAME")
 LOG_LEVEL = int(os.getenv("LOG_LEVEL"), logging.WARNING)
 
-log = logging.getLogger()
-log.setLevel(LOG_LEVEL)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def get_data_source_connection(
@@ -46,7 +48,7 @@ def read_data_from_source(
 ) -> pd.DataFrame:
     """Read data from data source"""
 
-    df = wr.mysql.read_sql_sql_query(sql_query, con=connection)
+    df = wr.mysql.read_sql_query(sql_query, con=connection)
     return df
 
 
@@ -69,45 +71,55 @@ def update_offset(client, parameter_name: str, new_value: int) -> None:
     client.put_parameter(Name=parameter_name, Value=new_value, Overwrite=True)
 
 
-def lambda_handler(event: dict, context) -> None:
+def lambda_handler(event: dict, context: LambdaContext) -> None:
+    """
+    TODO:
+        -> create a table manually
+        -> add partition if necessary
+        -> add xray to package
+        -> fix ssm, get parameter timeout
+    """
     try:
         parameter_client = boto3.client("ssm")
 
         columns = "*"
-        offset = get_last_offset(parameter_client, PARAMETER_NAME)
-        log.info(f"OFFSET recuperado: {offset}")
+        offset = get_last_offset(parameter_client, OFFSET_PARAMETER_NAME)
+        logger.info(f"OFFSET recuperado: {offset}")
         sql_query = f"SELECT {columns} FROM transactions ORDER BY `date` ASC LIMIT {LIMIT} OFFSET {offset}"
 
-        log.info(f"Quey: {json.dumps(sql_query)}")
-        log.info("Recuperando conexão com data source")
+        logger.info(f"Quey: {json.dumps(sql_query)}")
+        logger.info("Recuperando conexão com data source")
         connection = get_data_source_connection(
             MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_DATABASE, MYSQL_PORT
         )
 
-        log.info("Recuperando dados da origem")
+        logger.info("Recuperando dados da origem")
         df = read_data_from_source(connection, sql_query)
 
-        if df.size == 0:
-            log.info("Origem não retornou dados")
+        df_size = len(df.index)
+        if df_size == 0:
+            logger.info("Origem não retornou dados")
             return None
 
-        log.info(f"{df.size} linha(s) retornada(s) da origem")
+        logger.info(f"{df_size} linha(s) retornada(s) da origem")
 
-        log.info("Armazenando dados no destino")
+        logger.info(f"Armazenando dados no destino: {DESTINATION_PATH}")
         save_df_to_destination_source(df, DESTINATION_PATH)
 
-        new_offset_value = offset + df.size
-        log.info(f"Novo OFFSET: {new_offset_value}")
-        update_offset(parameter_client, PARAMETER_NAME, new_offset_value)
+        new_offset_value = offset + df_size
+        logger.info(f"Novo OFFSET: {new_offset_value}")
+        update_offset(parameter_client, OFFSET_PARAMETER_NAME, new_offset_value)
 
         return None
 
     except Exception as e:
-        log.error("Evento de entrada:")
-        log.error(event)
-        log.error("Contexto da funcao:")
-        log.error(context)
-        log.error("Erro:")
-        log.error(e)
+        logger.info("## EVENT")
+        logger.info(event)
+        logger.info("## LOG STREAM")
+        logger.info(context.log_stream_name)
+        logger.info("## OS ENVIRONMENTS")
+        logger.info(os.environ)
+        logger.error("## ERROR")
+        logger.error(e)
 
         raise e
